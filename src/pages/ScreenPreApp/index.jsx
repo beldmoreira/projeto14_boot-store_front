a61d7b780ea91e2ed64cd93a2bc293f8c61aa7c9
Isabela <isabela_cmoreira@hotmail.com>
2022-05-11 17:32:04 -0300

    feature: first screen finished

---
+++ src/pages/ScreenPreApp/index.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { Button, Container, LowerPart, Title } from "./style";
+import Footpic from "../../assets/footpic.png";
+
+
+export default function ScreenPreApp(){
+    const navigate = useNavigate();
+    return(
+        <Container>
+            <img alt="footpic.png" src={Footpic}/>
+            <LowerPart>
+                <Title> Find Your Best Collections </Title>
+                <Button onClick={() => navigate("/login")}> GET STARTED </Button>
+            </LowerPart>
+        </Container>
+
+    )
+}
---
+++ src/pages/ScreenPreApp/style.jsx
@@ -0,0 +1,68 @@
+import styled  from "styled-components";
+
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #2C8FEC;
+    height:100%;
+    
+    img {
+        position: absolute;
+        top: 0;
+        height: 62%;
+    }
+    
+`;
+const Title = styled.span`
+    font-family: gilroyextrabold;
+    font-size: 6vmin;
+    font-weight: 800;
+    line-height: 29px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #FFFFFF;
+`;
+
+const Button = styled.button`
+    height: 11vmin;
+    width: 40vmin;
+    border-radius: 50px;
+    border:none;
+    margin-bottom: 48px;
+    margin-top:40px;
+    gap:10px;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+
+    background: #FFFFFF;
+    font-family: Inter;
+    font-size: 3.5vmin;
+    font-weight: 500;
+    line-height: 19px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #2B8FEC;
+    
+`;
+const LowerPart = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align:center;
+    bottom:0;
+    position: absolute;
+`;
+
+export{
+    Container,
+    Title,
+    Button,
+    LowerPart
+}
