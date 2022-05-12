3cd9109c24eef11af8749fc1225e6a775a4bf22f
Isabela <isabela_cmoreira@hotmail.com>
2022-05-12 13:33:15 -0300

    Created a file with all the axios requests

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,2 +1,39 @@
 import axios from 'axios';
 
+const BASE_URL = 'http://localhost:5000';
+
+function createConfig(token) {
+  return {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  }
+}
+
+async function login(data) {
+    const token = await axios.post(`${BASE_URL}/login`, data);
+    return token;
+}
+
+const api = {
+    createConfig,
+    login
+}
+
+export default api;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
